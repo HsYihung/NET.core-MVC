d960a14d6d96340b4764b3018577de3ffc8da562
cc <a125012239@gmail.com>
2022-06-27 16:30:06 +0800

    20220627

---
+++ 上課/CustomerOrder2019/CustomerOrder2019/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using CustomerOrder2019.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerOrder2019.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            NorthwindContext dc = new NorthwindContext();
+            ViewBag.CustomerID = new SelectList(dc.Customers,"CustomerId","CompanyName" );
+            return View();
+        }
+
+        public async Task<IActionResult> Orders(string id)
+        {
+            NorthwindContext dc = new NorthwindContext();
+            Customers c = await dc.Customers.FindAsync(id);
+            return PartialView("_OrdersPartal", c.Orders);
+        }
+    }
+}
---
+++ 上課/CustomerOrder2019/CustomerOrder2019/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function UpdateOrders() {
+    $.ajax({
+        type: "Get",
+        /*url: "Customers/Orders/" + CustomerId.value 發行會失敗*/
+        url: webroot + CustomerId.value
+    }).done(function (data) {
+        $("#Orders").html(data);
+    }).fail(function (err) {
+        alert(err.responseText);
+    });
+}
